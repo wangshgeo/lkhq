f7e5cb799965a24803e18a788e4d8be67ed957ea
Robert Lee <rlee32@gatech.edu>
2019-10-27 10:36:56 -0400

    return points with shorter edge opportunities

--- research.hh
+++ research.hh
@@ -9,8 +9,9 @@
 
 namespace research {
 
-inline void shorter_edge_opportunities(const PointSet &point_set, const Tour &tour) {
+inline std::vector<primitives::point_id_t> shorter_edge_opportunities(const PointSet &point_set, const Tour &tour) {
     std::vector<size_t> shorter_edge_count;
+    std::vector<primitives::point_id_t> points_with_shorter_edges;
     for (primitives::point_id_t i{0}; i < point_set.size(); ++i) {
         const auto &next_length = tour.length(i);
         const auto &prev_length = tour.prev_length(i);
@@ -31,6 +32,7 @@ inline void shorter_edge_opportunities(const PointSet &point_set, const Tour &to
             continue;
         }
         shorter_edge_count.push_back(filtered_points.size());
+        points_with_shorter_edges.push_back(i);
     }
     std::cout << "ratio of points with shorter edge opportunities: "
         << static_cast<double>(shorter_edge_count.size()) / point_set.size()
@@ -38,6 +40,9 @@ inline void shorter_edge_opportunities(const PointSet &point_set, const Tour &to
     const auto &sum = std::accumulate(std::cbegin(shorter_edge_count), std::cend(shorter_edge_count), static_cast<double>(0.0));
     std::cout << "average shorter edge opportunities (per point with any opportunities): "
         << sum / shorter_edge_count.size() << std::endl;
+    const auto &max_ops = *std::max_element(std::cbegin(shorter_edge_count), std::cend(shorter_edge_count));
+    std::cout << "maximum shorter edge opportunities given any single point: " << max_ops << std::endl;
+    return points_with_shorter_edges;
 }
 
 }  // namespace research
